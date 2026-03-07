95bcc7a7abac4fc8f76ebc6d313fe1983e3040c3
abhi001-q <abhishek987ff@gmail.com>
2026-03-07 23:17:25 +0545

    Refactor Header component: improve code formatting and update responsive behavior for navigation items

--- src/components/Nav/Header.jsx
+++ src/components/Nav/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import logo from "../../assets/logo_1.png";
 import HeaderItem from "./HeaderItem";
 import {
@@ -8,11 +9,10 @@ import {
   HiPlayCircle,
   HiTv,
 } from "react-icons/hi2";
-import { HiPlus,HiDotsVertical } from "react-icons/hi";
+import { HiPlus, HiDotsVertical } from "react-icons/hi";
 
 function Header() {
-
-  const [toggle,settoggle]=useState(false);
+  const [toggle, settoggle] = useState(false);
   const navItems = [
     {
       name: "HOME",
@@ -40,21 +40,28 @@ function Header() {
     <div className="flex items-center  justify-between p-5">
       <div className="flex items-center gap-8">
         <img className=" w-20 md:w-30  object-cover" src={logo} alt="Logo" />
-        <div className='hidden   mb:flex gap-8'>
+        <div className="hidden   md:flex gap-8">
           {navItems.map((item) => (
             <HeaderItem name={item.name} Icon={item.icon} />
           ))}
         </div>
-          <div className='flex md:hidden gap-5'>
-          {navItems.map((item,index) => index<3&& (
-            <HeaderItem name={''} Icon={item.icon} />
-          ))}
-          <div className='md:hidden' onClick={()=>settoggle(!toggle)}>
-            <HeaderItem name={''} Icon={HiDotsVertical} />
-            {toggle? <div className='absolute mt-3 bg-[#121212] px-5 pb-4 border-gray-700 p-3'>
-               {navItems.map((item,index) => index>2&& (
-            <HeaderItem name={'item.name'} Icon={item.icon} />   ))}
-            </div>:null}
+        <div className="flex md:hidden gap-5">
+          {navItems.map(
+            (item, index) =>
+              index < 3 && <HeaderItem name={""} Icon={item.icon} />,
+          )}
+          <div className="md:hidden" onClick={() => settoggle(!toggle)}>
+            <HeaderItem name={""} Icon={HiDotsVertical} />
+            {toggle ? (
+              <div className="absolute mt-3 bg-[#121212] px-5 pb-4 border-gray-700 p-3">
+                {navItems.map(
+                  (item, index) =>
+                    index > 2 && (
+                      <HeaderItem name={item.name} Icon={item.icon} />
+                    ),
+                )}
+              </div>
+            ) : null}
           </div>
         </div>
       </div>
