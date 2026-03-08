a35e7b9c76567212a36ebf6e5378deebccd69898
abhi001-q <abhishek987ff@gmail.com>
2026-03-08 16:08:37 +0545

    Fix incorrect reference to movieBaseUrl in getTrendingVideos API call

--- src/services/globalApi.js
+++ src/services/globalApi.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const movieBaseUrl = 'https://api.themoviedb.org/3';
 const api_key='4e3a5de81dd6769c1788b1d584fa91f2'
 
-const getTrendingVideos=axios.get(this.movieBaseUrl+
-    "/trending/all/day?api_key"+api_key);
+const getTrendingVideos=axios.get(movieBaseUrl+
+    "/trending/all/day?api_key="+api_key);
 
 
 export default {
